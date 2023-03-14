844f0e82b856b37fd1552e011337f38b370f0e63
albrecht-net <jonasalbrecht9@gmail.com>
2023-03-14 20:49:38 +0100

    Added default core files include, change method verify

--- api/user/login.php
+++ api/user/login.php
@@ -1,26 +1,29 @@
 <?php
-// Allowed request types
-$_headerAllowMethod = array('POST');
-
-
-if(in_array($_SERVER['REQUEST_METHOD'], $_headerAllowMethod)) {
-    session_start();
-    header('Content-Type: application/json');
-    $_response = array(
-        'success' => false,
-        'message' => array(
-            'messageBarType' => 2,
-            'messageBarDismisible' => true,
-            'messageTitle' => 'Tiitel',
-            'messageBody' => session_id()
-        ),
-        'data' => $_POST,
-        'cookie' => $_COOKIE
-    );
-    
-    echo json_encode($_response);
-
-} else {
-    http_response_code(405);
-    header('Allow: POST');
+if (!defined('ROOT_PATH')) {
+    define('ROOT_PATH', dirname(__FILE__, 3) . DIRECTORY_SEPARATOR);
 }
+require_once ROOT_PATH . 'includes' . DIRECTORY_SEPARATOR . 'core' . DIRECTORY_SEPARATOR . 'init.php';
+
+// Allowed request types
+$access_control_allow_methods = array('POST');
+
+// Verify if requested method is in allowed ones
+if(!in_array($_SERVER['REQUEST_METHOD'], $access_control_allow_methods)) {
+    http_response_code(405);
+    header('Allow: ' . implode(', ', $access_control_allow_methods));
+}
+
+
+header('Content-Type: application/json');
+$response = array(
+    'success' => false,
+    'message' => array(
+        'messageBarType' => 2,
+        'messageBarDismisible' => true,
+        'messageTitle' => 'Tiitel'
+    ),
+    'data' => $_POST,
+    'cookie' => $_COOKIE
+);
+
+echo json_encode($response);
