2960ebfc7e1d30eecd3f06fdc6657e05f58d6e35
albrecht-net <albrechtjonas@outlook.com>
2023-09-10 15:12:33 +0200

    Replaced one trigger_error with throwable, comment formating

--- includes/classes/db.php
+++ includes/classes/db.php
@@ -13,12 +13,12 @@ class db {
     private static $_instance_usr_link = null;
 
     /**
-     * @var int $_mode Mode selector, -1: System database, otherwise user_id (int != -1)
+     * @var int        $_mode          Mode selector, -1: System database, otherwise user_id (int != -1)
      */
     private $_mode;
 
     /**
-     * @var object $_mysqli Object which represents the connection to the MySQL Server.
+     * @var object     $_mysqli        Object which represents the connection to the MySQL Server.
      */
     private $_mysqli;
     
@@ -26,12 +26,12 @@ class db {
     private $_result;
 
     /**
-     * @var int $errno Error code for the most recent function call
+     * @var int        $errno          Error code for the most recent function call
      */
     public $errno;
 
     /**
-     * @var string $error Description for last statement error
+     * @var string     $error          Description for last statement error
      */
     public $error;
 
@@ -39,8 +39,8 @@ class db {
      * Entrypoint for every mysql connection.
      * When the selected database is called up for the first time, a new instance will get created.
      * 
-     * @param int $mode -1: System database with credentials from config. Otherwise user_id (int != -1) for user database with credentials from system database.
-     * @return object Return the instantiated object of the choosen database.
+     * @param int      $mode           -1: System database with credentials from config. Otherwise user_id (int != -1) for user database with credentials from system database.
+     * @return object                  Return the instantiated object of the choosen database.
      */
     public static function init(int $mode = -1) {
         if ($mode == -1) {
@@ -52,7 +52,10 @@ class db {
             if (!isset(self::$_instance_sys_link)) {
                 self::$_instance_sys_link = new self(-1);
             }
-            if (!isset(self::$_instance_usr_link) | self::$_instance_usr_link->_mode !== $mode) {
+            if (!isset(self::$_instance_usr_link)) {
+                self::$_instance_usr_link = new self($mode);
+            }
+            if (isset(self::$_instance_usr_link) && self::$_instance_usr_link->_mode != $mode) {
                 self::$_instance_usr_link = new self($mode);
             }
             return self::$_instance_usr_link;
@@ -62,15 +65,15 @@ class db {
     /**
      * Constructor
      * 
-     * @param int $mode 0: System database with credentials from config. Otherwise user_id (int != 0) for user database with credentials from system database.
-     * @return void No value is returned
+     * @param int      $mode           0: System database with credentials from config. Otherwise user_id (int != 0) for user database with credentials from system database.
+     * @return void                    No value is returned
      */
     private function __construct(int $mode) {
         $this->_mode = $mode;
         if ($mode == -1) {
             $this->_connect_sys_db();
         } else {
-                        $this->_connect_usr_db();
+            $this->_connect_usr_db();
         }
     }
 
@@ -78,7 +81,7 @@ class db {
      * Open a new connection to the MySQL server for system database.
      * For the database credentials the entries from the config will be used.
      * 
-     * @return void No value is returned
+     * @return void                    No value is returned
      */
     private function _connect_sys_db() {
         $this->_mysqli = new mysqli(config::get('db.host'), config::get('db.username'), config::get('db.password'), config::get('db.name'), config::get('db.port'));
@@ -98,7 +101,7 @@ class db {
      * Open a new connection to the MySQL server for user database.
      * For the database credentials the userID (stored in $_mode) will be used.
      * 
-     * @return void No value is returned
+     * @return void                    No value is returned
      */
     private function _connect_usr_db() {
         self::$_instance_sys_link->run_query("SELECT db_host, db_port, db_username, db_password, db_name FROM `databases` WHERE user_id=? LIMIT 1", "i", $this->_mode);
@@ -112,7 +115,7 @@ class db {
         $this->errno = $this->_mysqli->connect_errno;
 
         if ($this->_mysqli->connect_errno) {
-            trigger_error('User #' . $this->_mode . ' cannot connect to user database. MySQL said: #' . mysqli_connect_errno() . ' - ' . mysqli_connect_error(), E_USER_NOTICE);
+            throw new exception_usr_link('User #' . $this->_mode . ' cannot connect to user database. MySQL said: #' . mysqli_connect_errno() . ' - ' . mysqli_connect_error(), mysqli_connect_errno());
             return;
         }
 
@@ -124,12 +127,13 @@ class db {
     /**
      * Wrapper for mysqli_stmt::prepare, mysqli_stmt::bind_param and mysqli_stmt::execute, creates a mysqli_result
      * 
-     * @param string $query The query, as a string. It must consist of a single SQL statement.
-     * @param string $types A string that contains one or more characters which specify the types for the corresponding bind variables.
-     * @param mixed $var Parameter
-     * @param mixed $vars Additional parameters. The number of variables and length of string types must match the parameters in the statement.
-     * @throws exception_sys_link|exception_usr_link
-     * @return bool Returns true on success or false on failure.
+     * @param string   $query          The query, as a string. It must consist of a single SQL statement.
+     * @param string   $types          Optional. A string that contains one or more characters which specify the types for the corresponding bind variables.
+     * @param mixed    $var            Optional (Required if $types is set). Parameter. The number of variables and length of string $types must match the parameters in the statement.
+     * @param mixed    $vars           Optional  (Required if $types is set). Additional parameters. The number of variables and length of string $types must match the parameters in the statement.
+     * @throws                         exception_sys_link if mode selector is equal to -1
+     * @throws                         exception_usr_link if mode selector is not equal to -1
+     * @return bool                    Returns true on success or false on failure.
      */
     public function run_query(string $query, string $types = null, &$var = null, &...$vars) {
         if (!$this->_stmt->prepare($query)) {
@@ -191,7 +195,7 @@ class db {
     /**
      * Gets the number of rows in a result.
      * 
-     * @return integer Number of rows in the result set.
+     * @return integer                 Number of rows in the result set.
      */
     public function count() {
         return $this->_result->num_rows;
@@ -200,7 +204,7 @@ class db {
     /**
      * Fetches all result rows as an associative array.
      * 
-     * @return array Result of all rows as array.
+     * @return array                   Result of all rows as array.
      */
     public function fetch_array() {
         return $this->_result->fetch_all(MYSQLI_ASSOC);
@@ -209,8 +213,8 @@ class db {
     /**
      * Fetches one result row as an associative array.
      * 
-     * @param int $offset Adjusts the result pointer to the given offset and return one result row as array
-     * @return array|void|bool Returns an array representing the fetched row, null if there are no more rows in the result set, or false on failure.
+     * @param int      $offset         Adjusts the result pointer to the given offset and return one result row as array
+     * @return array|void|bool         Returns an array representing the fetched row, null if there are no more rows in the result set, or false on failure.
      */
     public function fetch_one(int $offset = 0) {
         if (!$this->_result->data_seek($offset)) {
