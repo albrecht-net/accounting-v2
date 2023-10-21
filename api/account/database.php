069314de0aa2ed22ee7b6254974fe6110fd68b8a
albrecht-net <albrechtjonas@outlook.com>
2023-10-21 16:11:09 +0200

    User database connection test

--- api/account/database.php
+++ api/account/database.php
@@ -16,6 +16,39 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 
 // Replace current user database configuration and verify connection
 } elseif ($_SERVER['REQUEST_METHOD'] == 'PUT') {
+    try {
+        $request_data = array(
+            'db_host' => trim(request::body('db_host')),
+            'db_port' => trim(request::body('db_port')),
+            'db_username' => trim(request::body('db_username')),
+            'db_password' => request::body('db_password'),
+            'db_name' => trim(request::body('db_name')),
+            'force' => empty(request::body('force')) ? false : boolval(request::body('force'))
+        );
+    } catch (JsonException $e) {
+        response::error('Missing request data.');
+        response::send(false, 400);
+        exit;
+    }
+
+    // Open temporary connection to user database
+    $_tmp_mysqli = new mysqli($request_data['db.host'], $request_data['db.username'], $request_data['db.password'], $request_data['db.name'], $request_data['db.port']);
+
+    if ($_tmp_mysqli->connect_errno) {
+        // Close temporary connection
+        $_tmp_mysqli->close();
+
+        response::error("Cannot connect to system database, check config.php. MySQL said: #" . mysqli_connect_errno() . " - " . mysqli_connect_error(), mysqli_connect_errno());
+
+        if ($request_data['force'] == false) {
+            response::send(false, 400);
+            exit;
+        }
+    } else {
+        // Close temporary connection
+        $_tmp_mysqli->close();
+    }
+
 
 // Delete current user database configuration
 } elseif ($_SERVER['REQUEST_METHOD'] == 'DELETE') {
