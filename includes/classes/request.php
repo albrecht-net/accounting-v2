0fe92da23b3bd5ccdac4add0f91e974defbfdd11
albrecht-net <albrechtjonas@outlook.com>
2024-02-02 21:51:19 +0100

    Added request data input filtering

--- includes/classes/request.php
+++ includes/classes/request.php
@@ -33,6 +33,7 @@ class request {
         if (!empty($_body_raw)) {
             self::$_body = json_decode($_body_raw, true, 512, JSON_THROW_ON_ERROR);
         }
+
         return;
     }
 
@@ -42,18 +43,31 @@ class request {
      * @param string   $name           Name of a value in the query string
      * @param bool     $required       Throws a exception if set to true, but no value was found in the query.
      * @param bool     $trim           Optional. Strip whitespace from the beginning and end before return query value.
+     * @param integer  $filter         Optional. The ID of the filter to apply before return value. If omitted, FILTER_DEFAULT will be used, which is equivalent to FILTER_UNSAFE_RAW.
+     * @param array|int $options       Optional. Associative array of options or bitwise disjunction of flags.
      * @throws                         exception_request if the query parameter with $name is required but was not found.
      * @return mixed|void              Query string value, or void if name not found.
      */
-    public static function query_str($name, $required, $trim = true) {
+    public static function query_str($name, $required, $trim = true, $filter = FILTER_DEFAULT, $options = 0) {
         if (count(self::$_query_str) == 0) {
             self::load_query_str();
         }
 
-        if (isset(self::$_query_str[$name]) && $trim) {
-            return trim(self::$_query_str[$name]);
-        } elseif ((isset(self::$_query_str[$name]))) {
-            return self::$_query_str[$name];
+        $_req = self::$_query_str[$name];
+
+        if (isset($_req)) {
+            if ($trim) {
+                $_req = trim($_req);
+            }
+
+            $_req = filter_var($_req, $filter, $options);
+
+            if (($_req === false && $filter !== FILTER_VALIDATE_BOOL) || ($_req === null && $filter === FILTER_VALIDATE_BOOL)) {
+                throw new exception_request("Query parameter \"" . $name . "\" does not match required format.");
+            } else {
+                return $_req;
+            }
+
         } elseif ($required) {
             throw new exception_request("Query parameter \"" . $name . "\" was not provided in URL but is required.");
         }
@@ -67,18 +81,31 @@ class request {
      * @param string   $name           Name of a value in the request body
      * @param bool     $required       Throws a exception if set to true, but no value was found in the query.
      * @param bool     $trim           Optional. Strip whitespace from the beginning and end before return query value.
+     * @param integer  $filter         Optional. The ID of the filter to apply before return value. If omitted, FILTER_DEFAULT will be used, which is equivalent to FILTER_UNSAFE_RAW.
+     * @param array|int $options       Optional. Associative array of options or bitwise disjunction of flags.
      * @throws                         exception_request if the query parameter with $name is required but was not found.
      * @return mixed|void              Request body value, or void if name not found.
      */
-    public static function body($name, $required, $trim = true) {
+    public static function body($name, $required, $trim = true, $filter = FILTER_DEFAULT, $options = 0) {
         if (count(self::$_body) == 0) {
             self::load_body();
         }
 
-        if (isset(self::$_body[$name]) && $trim) {
-            return self::$_body[$name];
-        } elseif ((isset(self::$_body[$name]))) {
-            return self::$_body[$name];
+        $_req = self::$_body[$name];
+
+        if (isset($_req)) {
+            if ($trim) {
+                $_req = trim($_req);
+            }
+
+            $_req = filter_var($_req, $filter, $options);
+
+            if (($_req === false && $filter !== FILTER_VALIDATE_BOOL) || ($_req === null && $filter === FILTER_VALIDATE_BOOL)) {
+                throw new exception_request("Query parameter \"" . $name . "\" does not match required format.");
+            } else {
+                return $_req;
+            }
+
         } elseif ($required) {
             throw new exception_request("Query parameter \"" . $name . "\" was not provided in request body but is required.");
         }
