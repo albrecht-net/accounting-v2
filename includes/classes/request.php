40d43faf80f9fd00158e8f2995dd37e20e3aaf54
albrecht-net <albrechtjonas@outlook.com>
2024-05-14 14:44:12 +0200

    Add option to set default value which will be used, if no value was provided in request

--- includes/classes/request.php
+++ includes/classes/request.php
@@ -43,8 +43,10 @@ class request {
      * @param string   $name           Name of a value in the query string
      * @param bool     $required       Throws a exception if set to true, but no value was found in the query.
      * @param bool     $trim           Optional. Strip whitespace from the beginning and end before return query value.
-     * @param integer  $filter         Optional. The ID of the filter to apply before return value. If omitted, FILTER_DEFAULT will be used, which is equivalent to FILTER_UNSAFE_RAW.
+     * @param integer  $filter         Optional. The ID of the filter to apply before return value. If omitted, FILTER_DEFAULT will be used,
+     *                                 which is equivalent to FILTER_UNSAFE_RAW.
      * @param array|int $options       Optional. Associative array of options or bitwise disjunction of flags.
+     *                                 The default value provided in ['options']['default'] will not overwrite the value if the filter fails.
      * @throws                         exception_request if the query parameter with $name is required but was not found.
      * @return mixed|void              Query string value, or void if name not found.
      */
@@ -53,6 +55,7 @@ class request {
             self::load_query_str();
         }
 
+        // Filter value if one is provied in query string
         if (isset(self::$_query_str[$name])) {
             $_request = self::$_query_str[$name];
 
@@ -61,23 +64,34 @@ class request {
             }
 
             // Add FILTER_NULL_ON_FAILURE flag as option if filter type is set to FILTER_VALIDATE_BOOL
+            // Overwrite default value to catch filter failures
             if ($filter === FILTER_VALIDATE_BOOL) {
                 $options['flags'] |= FILTER_NULL_ON_FAILURE;
+                $options['options']['default'] = null;
+            } else {
+                $options['options']['default'] = false;
             }
 
             $_request = filter_var($_request, $filter, $options);
 
             if (($_request === false && $filter !== FILTER_VALIDATE_BOOL) || ($_request === null && $filter === FILTER_VALIDATE_BOOL)) {
                 throw new exception_request("Query parameter \"" . $name . "\" does not match required format.");
-            } else {
-                return $_request;
             }
 
+        // Throw exception if value was not set but is required
         } elseif ($required) {
             throw new exception_request("Query parameter \"" . $name . "\" was not provided in URL but is required.");
+
+        // Return default value if value was not set
+        } elseif (isset($options['options']['default'])) {
+            $_request = $options['options']['default'];
+
+        // Return void if value was not set, not required and no default value was provided
+        } else {
+            $_request = null;
         }
 
-        return;
+        return $_request;
     }
 
     /**
@@ -86,8 +100,10 @@ class request {
      * @param string   $name           Name of a value in the request body
      * @param bool     $required       Throws a exception if set to true, but no value was found in the query.
      * @param bool     $trim           Optional. Strip whitespace from the beginning and end before return query value.
-     * @param integer  $filter         Optional. The ID of the filter to apply before return value. If omitted, FILTER_DEFAULT will be used, which is equivalent to FILTER_UNSAFE_RAW.
+     * @param integer  $filter         Optional. The ID of the filter to apply before return value. If omitted, FILTER_DEFAULT will be used,
+     *                                 which is equivalent to FILTER_UNSAFE_RAW.
      * @param array|int $options       Optional. Associative array of options or bitwise disjunction of flags.
+     *                                 The default value provided in ['options']['default'] will not overwrite the value if the filter fails.
      * @throws                         exception_request if the query parameter with $name is required but was not found.
      * @return mixed|void              Request body value, or void if name not found.
      */
@@ -96,6 +112,7 @@ class request {
             self::load_body();
         }
 
+        // Filter value if one is provied in request body
         if (isset(self::$_body[$name])) {
             $_request = self::$_body[$name];
 
@@ -103,18 +120,34 @@ class request {
                 $_request = trim($_request);
             }
 
+            // Add FILTER_NULL_ON_FAILURE flag as option if filter type is set to FILTER_VALIDATE_BOOL
+            // Overwrite default value to catch filter failures
+            if ($filter === FILTER_VALIDATE_BOOL) {
+                $options['flags'] |= FILTER_NULL_ON_FAILURE;
+                $options['options']['default'] = null;
+            } else {
+                $options['options']['default'] = false;
+            }
+
             $_request = filter_var($_request, $filter, $options);
 
             if (($_request === false && $filter !== FILTER_VALIDATE_BOOL) || ($_request === null && $filter === FILTER_VALIDATE_BOOL)) {
                 throw new exception_request("Query parameter \"" . $name . "\" does not match required format.");
-            } else {
-                return $_request;
             }
 
+        // Throw exception if value was not set but is required
         } elseif ($required) {
             throw new exception_request("Query parameter \"" . $name . "\" was not provided in request body but is required.");
+
+        // Return default value if value was not set
+        } elseif (isset($options['options']['default'])) {
+            $_request = $options['options']['default'];
+
+        // Return void if value was not set, not required and no default value was provided
+        } else {
+            $_request = null;
         }
 
-        return;
+        return $_request;
     }
 }
