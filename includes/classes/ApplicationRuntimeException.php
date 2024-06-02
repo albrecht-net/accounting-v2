3734c7bddc9d52c88c33bb1ec56eb1cfb1625b35
albrecht-net <albrechtjonas@outlook.com>
2024-06-02 20:45:27 +0200

    Added custom exception for errors during runtime of application

---
+++ includes/classes/ApplicationRuntimeException.php
@@ -0,0 +1,8 @@
+<?php
+// Runtime exception which additionally pushes exception messages to the logfile
+class ApplicationRuntimeException extends RuntimeException {
+    public function __construct(string $message = "", int $code = 0, ?Throwable $previous = null) {
+        trigger_error("ApplicationRuntimeException #" . $code . " - UID: "  . USER_ID . " - " . $message, E_USER_NOTICE);
+        parent::__construct($message, $code, $previous);
+    }
+}
